e05f062dc06cc6dc3cff90a92b54178a7ba7c8f5
Giovanna Pessoa <gipessoa12@gmail.com>
2024-06-05 18:14:22 -0300

    codigo funcionamento

--- contador_medidas.py
+++ contador_medidas.py
@@ -8,3 +8,20 @@ def centimetrosParaMetros(centimetros):
 
 def metrosParaCentimetros(metros):
     return metros * 100
+print("Conversor de Medidas")
+print("1. Converter de centímetros para metros")
+print("2. Converter de metros para centímetros")
+while True:
+        escolha = input("Escolha uma opção: ")
+        if escolha == "1":
+         centimetros = float(input("Digite o valor em centímetros: "))
+         metros = CentimetrosParaMetros(centimetros)
+         print(f"{centimetros} centímetros é igual a {metros} metros")
+        elif escolha == "2":
+            metros = float(input("Digite o valor em metros: "))
+            centimetros = MetrosParaCentimetros(metros)
+            print(f"{metros} metros é igual a {centimetros} centímetros")
+        elif escolha=="sair":
+         break
+        else:
+            print("Opção inválida. Por favor, tente novamente.")
