f2c02d4c7bd5a61a53eec0ef4890b66d2ad76200
Giovanna Pessoa <gipessoa12@gmail.com>
2024-06-05 18:09:49 -0300

    Class medidas

---
+++ contador_medidas.py
@@ -0,0 +1,10 @@
+# conversoes.py
+class ConversorMedidas:
+    def __init__(self):
+        pass
+
+def centimetrosParaMetros(centimetros):
+    return centimetros / 100
+
+def metrosParaCentimetros(metros):
+    return metros * 100
