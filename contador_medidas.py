ed9d9114ef3156ace126ef93af0597e16067724c
Giovanna Pessoa <gipessoa12@gmail.com>
2024-06-12 17:17:25 -0300

    Refeito codigo temperatura

--- contador_medidas.py
+++ contador_medidas.py
@@ -1,10 +1,9 @@
-# conversoes.py
-class ConversorMedidas:
-    def __init__(self):
+class Conversor_medidas:
+    def _init_(self):
         pass
 
-def centimetrosParaMetros(centimetros):
-    return centimetros / 100
+    def centimetrosParaMetros(centimetros):
+        return centimetros / 100
 
 def metrosParaCentimetros(metros):
     return metros * 100
@@ -12,16 +11,16 @@ print("Conversor de Medidas")
 print("1. Converter de centímetros para metros")
 print("2. Converter de metros para centímetros")
 while True:
-        escolha = input("Escolha uma opção: ")
-        if escolha == "1":
+     escolha = input("Escolha uma opção: ")
+     if escolha == "1":
          centimetros = float(input("Digite o valor em centímetros: "))
          metros = CentimetrosParaMetros(centimetros)
          print(f"{centimetros} centímetros é igual a {metros} metros")
-        elif escolha == "2":
+     elif escolha == "2":
             metros = float(input("Digite o valor em metros: "))
             centimetros = MetrosParaCentimetros(metros)
             print(f"{metros} metros é igual a {centimetros} centímetros")
-        elif escolha=="sair":
-         break
-        else:
+     elif escolha=="sair":
+            break
+     else:
             print("Opção inválida. Por favor, tente novamente.")
