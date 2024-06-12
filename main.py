434495db6264db01f7653ccedf928423d31f6641
Giovanna Pessoa <gipessoa12@gmail.com>
2024-06-12 17:18:16 -0300

    arquivo main com erro

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-def menu_principal():
-    print("CONVERSOR DE MEDIDAS")
-    print("Conversor de Temperatura")
-    print("3. sair")
-
-    opcao = input("digite o que deseja: ")
-    return opcao
+from contador_medidas import ConversorMedidas
+from temperatura import ConversorDeTemperatura
 
 def sair():
     print("encerrar programa")
+
+def menu_principal():
+    print("Conversor de Medidas")
+    print("Conversor de Temperatura")
+    print("Conversor de temperaturas")
+    print("3. sair")
+    opcao = input("digite o que deseja: ")
+    if opcao == "1":
+        ConversorMedidas. 
+
+    elif opcao == "2":
+    elif opcao == "3":
+        sair()
+        break
+    else
+
+
 
 escolha = menu_principal()
 
